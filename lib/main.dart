215392c3091192b410af8353a3786bce248fd2a4
MBMAZAM <maza_1506@hotmail.com>
2024-02-23 22:45:55 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
+//import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true, //Yo ya tenia Meterial 3, esto es para llamarlo por sino lo tengo 
+        colorSchemeSeed: Colors.green
+
+      ),
+      home: const CounterFunctionsScreen(),
+    );
+  }
+
+
+}
